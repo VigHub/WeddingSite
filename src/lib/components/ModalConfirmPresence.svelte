a05489dd4c2dc187480cdf6b79b1b49064b19173
vighub <gvigan97@gmail.com>
2023-11-06 01:14:57 +0100

    changed modal color to white

--- src/lib/components/ModalConfirmPresence.svelte
+++ src/lib/components/ModalConfirmPresence.svelte
@@ -8,10 +8,10 @@
 	let mail: string = '';
 </script>
 
-<div class="p-4 md:flex md:flex-col lg:w-1/2 w-full bg-surface-200 rounded-2xl">
+<div class="p-4 md:flex md:flex-col lg:w-1/2 w-full bg-white rounded-2xl">
 	<p class="mb-4">{name} {surname}</p>
 
-	<RadioGroup>
+	<RadioGroup background="bg-white">
 		<RadioItem bind:group={presence} name="justify" value={0}>In attesa</RadioItem>
 		<RadioItem bind:group={presence} name="justify" value={1}>Ci sono</RadioItem>
 		<RadioItem bind:group={presence} name="justify" value={2}>Non ci sono</RadioItem>
@@ -21,7 +21,6 @@
 		<form class="w-full space-y-2 mt-4">
 			<label for="message" class="block">Il tuo messaggio (opzionale, solo se vuoi)</label>
 			<input
-				type="mail"
 				id="mail"
 				class="w-full p-2 block rounded-lg focus:ring-blue-500 focus:border-blue-500"
 				placeholder="mail@esempio.com"
@@ -30,8 +29,7 @@
 			<textarea
 				id="message"
 				rows="4"
-				class="block p-2.5 w-full rounded-lg
-			focus:ring-blue-500 focus:border-blue-500"
+				class="block p-2.5 w-full rounded-lg focus:ring-blue-500 focus:border-blue-500"
 				placeholder="Scrivi qui il tuo messaggio"
 			/>
 			<button type="submit" class="btn variant-filled align-baseline float-right">Invia</button>
