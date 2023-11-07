f4f49576ad749b96e6cba80ee4561ad4b219bb93
gianluca.vigano <gianluca.vigano@it.abb.com>
2023-11-07 17:40:06 +0100

    introduced Supabase

--- src/lib/utils/searchGuests.ts
+++ src/lib/utils/searchGuests.ts
@@ -1,25 +1,22 @@
-import guests from "./guests.json"
-
 export interface GuestFile {
-    guests: Guest[]
+	guests: Guest[];
 }
 
 export interface Guest {
-    name: string,
-    surname: string
+	id: number;
+	name: string;
+	surname: string;
+	confirmation: number;
 }
 
-
-
-export function searchGuest(name: string, surname: string) {
-    name = name.trim();
-    surname = surname.trim();
-    if (name === '' && surname === '')
-        return []
-    const guestFound = (guests as GuestFile).guests.filter(guest => {
-        const guestName = guest.name.toLowerCase().startsWith(name.toLowerCase())
-        const guestSurname = guest.surname.toLowerCase().startsWith(surname.toLowerCase())
-        return guestName && guestSurname
-    })
-    return guestFound.slice(0, 5)
+export function searchGuest(guests: Guest[], name: string, surname: string, limit: number = 5) {
+	name = name.trim();
+	surname = surname.trim();
+	if (name === '' && surname === '') return [];
+	const guestFiltered = guests.filter((guest) => {
+		const guestName = guest.name.toLowerCase().startsWith(name.toLowerCase());
+		const guestSurname = guest.surname.toLowerCase().startsWith(surname.toLowerCase());
+		return guestName && guestSurname;
+	});
+	return guestFiltered.slice(0, limit);
 }
