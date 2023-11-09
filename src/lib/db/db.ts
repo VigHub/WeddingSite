6eb4246a68c8ebc0d50525d653070c61e8604461
gianluca.vigano <gianluca.vigano@it.abb.com>
2023-11-09 18:23:23 +0100

    changed how to retrieve guests confirmation and loading

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -36,12 +36,14 @@ interface GuestMessageWithGuest {
 }
 
 export const getGuestsbyNameSurname = async (name: string, surname: string) => {
+	if (name === '' && surname === '') return [];
 	const { data } = await supabase
-		.from(GuestTable)
-		.select()
-		.ilike('name', `${name}%`)
-		.ilike('surname', `${surname}%`);
-	const guests: Guest[] = data ?? [];
+		.from(GuestAttendanceTable)
+		.select('attendance, guest:Guest!inner(id, name, surname)')
+		.ilike('guest.name', `${name}%`)
+		.ilike('guest.surname', `${surname}%`)
+		.returns<GuestAttendance[]>();
+	const guests = data ?? [];
 	return guests;
 };
 
