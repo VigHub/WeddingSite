1b7784c545711f995b44c57bbd9d4cb8bfc96103
vighub <gvigan97@gmail.com>
2023-11-13 23:11:14 +0100

    introduce error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
+<div class="space-y-8 text-center">
+	<p class="font-mono text-9xl">404</p>
+	<p class="font text-3xl">Pagina non trovata</p>
+	<p class="text-center">Ci dispiace, ma la pagina che stai cercando non esiste 😅</p>
+	<p class="text-center">
+		Clicca <a href="{base}/" class="text-primary-700">qui</a> per tornare alla home
+	</p>
+</div>
