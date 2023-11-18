858a3d2cd18319f7e0b7518cfb8276c156da922d
vighub <gianluca.vigano@it.abb.com>
2023-11-18 01:13:50 +0100

    added translations

--- src/lib/components/LanguageSelector.svelte
+++ src/lib/components/LanguageSelector.svelte
@@ -4,12 +4,22 @@
 
 	let localLanguage: string = $locale?.slice(0, 2) ?? 'it';
 	$: $locale = localLanguage;
+
+	const colors: Map<string, string> = new Map([
+		['it', 'bg-green-700'],
+		['en', 'bg-blue-800'],
+		['ja', 'bg-red-700']
+	]);
 </script>
 
-<RadioGroup background="bg-gray-200" border="border border-black" padding="p-1 px-2">
+<RadioGroup background="bg-gray-100" border="border border-black" padding="p-1 px-2">
 	{#each $locales as lcl}
-		<RadioItem bind:group={localLanguage} name="justify" value={lcl} class="text-xs"
-			>{lcl}</RadioItem
+		<RadioItem
+			bind:group={localLanguage}
+			name="justify"
+			value={lcl}
+			class="text-xs"
+			color={colors.get(lcl)}>{lcl}</RadioItem
 		>
 	{/each}
 </RadioGroup>
